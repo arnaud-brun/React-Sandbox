07f7b23ca6219374c294d85653d9afd68d266543
Arnaud <arnaud.brun.vidal@gmail.com>
2018-04-29 11:37:22 +0200

    Add socket.io and SocketMiddleware to the server

---
+++ server/middlewares/socketMiddleware.js
@@ -0,0 +1,24 @@
+/* eslint-disable global-require */
+
+/**
+ * Socket middleware
+ */
+module.exports = (app, port, logger) => {
+  const server = require('http').createServer(app);
+  const io = require('socket.io')(server);
+  server.listen(port);
+
+  // Create a chat on '/chat' with specific event
+  const chat = io.of('/chat');
+  const chatEvent = 'SOCKET_CHAT_EVENT';
+
+  chat.on('connection', (socket) => {
+    socket.broadcast.emit('New user connected');
+    socket.on(chatEvent, (message) => {
+      logger.log('MSG received from connected client: ');
+      logger.log(message);
+      socket.emit(chatEvent, message);
+      socket.broadcast.emit(chatEvent, message);
+    });
+  });
+};
