b6519f8ab336700b8acadb66807820f187ad8140
Arnaud <arnaud.brun.vidal@gmail.com>
2018-05-18 10:28:35 +0200

    INclude authentication basic logic for signin up and login

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,13 +1,53 @@
 /**
  * Authentication middleware
  */
+const crypto = require('crypto');
 const auth = require('express')();
 const validator = require('validator'); // https://www.npmjs.com/package/validator
+
+const SALT = 'SUPER_SECRET_SALT';
+
+function encrypt(key, data) {
+  const cipher = crypto.createCipher('aes-256-cbc', key);
+  let crypted = cipher.update(data, 'utf-8', 'hex');
+  crypted += cipher.final('hex');
+  return crypted;
+}
+
+function decrypt(key, data) {
+  const decipher = crypto.createDecipher('aes-256-cbc', key);
+  let decrypted = decipher.update(data, 'hex', 'utf-8');
+  decrypted += decipher.final('utf-8');
+  return decrypted;
+}
+
+function hashPassword(pwd) {
+  // Encrypt password with salt and crypto methods
+  return encrypt(pwd, SALT);
+}
+
+const users = [{
+    email: 'alice@sfr.fr',
+    username: 'alice',
+    password: hashPassword('alicealice'),
+  }, {
+    email: 'bob@sfr.fr',
+    username: 'bob',
+    password: hashPassword('bobbobbob'),
+  }, {
+    email: 'charly@sfr.fr',
+    username: 'charly',
+    password: hashPassword('charlycharly'),
+  }, {
+    email: 'daryl@sfr.fr',
+    username: 'daryl',
+    password: hashPassword('daryldaryl'),
+  }
+];
 
 function validateLoginForm(payload) {
   const errors = {};
   let isFormValid = true;
-  let message = '';
 
   if (!payload || typeof payload.username !== 'string' || payload.username.trim().length === 0) {
     isFormValid = false;
@@ -19,13 +59,8 @@ function validateLoginForm(payload) {
     errors.password = 'Password must have at least 8 characters.';
   }
 
-  if (!isFormValid) {
-    message = 'Check the form for errors.';
-  }
-
   return {
     success: isFormValid,
-    message,
     errors
   };
 }
@@ -33,7 +68,6 @@ function validateLoginForm(payload) {
 function validateSignupForm(payload) {
   const errors = {};
   let isFormValid = true;
-  let message = '';
 
   if (!payload || typeof payload.email !== 'string' || !validator.isEmail(payload.email)) {
     isFormValid = false;
@@ -50,46 +84,109 @@ function validateSignupForm(payload) {
     errors.password = 'Password must have at least 8 characters.';
   }
 
-  if (!isFormValid) {
-    message = 'Check the form for errors.';
-  }
-
   return {
     success: isFormValid,
-    message,
     errors
   };
 }
 
+function createUser(payload) {
+  // Payload is correct and contains email - username - password
+  const errors = {};
+  let isCreationValid = true;
+
+  // Check for already used email
+  if (users.filter(user => user.email == payload.email).length != 0) {
+    isCreationValid = false;
+    errors.email = 'Email already in use';
+  }
+
+  // Check for available username
+  if (users.filter(user => user.username == payload.username).length != 0) {
+    isCreationValid = false;
+    errors.username = 'Username already in use';
+  }
+
+  // Save object in db if possible
+  if (isCreationValid) {
+    users.push({
+      email: payload.email,
+      username: payload.username,
+      password: hashPassword(payload.password),
+    });
+  }
+
+  return {
+    success: isCreationValid,
+    errors
+  };
+}
+
+
+function getUser(payload) {
+  // Payload is correct and contains username - password
+  const errors = {};
+  let isGetValid = true;
+  const user = users.filter(user => user.username == payload.username);
+
+  if (user.length != 1) {
+    isGetValid = false;
+    errors.username = 'There was an error on username or password';
+    errors.password = 'There was an error on username or password';
+  }
+  else {
+    const retrievedUser = user[0];
+    if (retrievedUser.password !== hashPassword(payload.password)) {
+      isGetValid = false;
+      errors.username = 'There was an error on username or password';
+      errors.password = 'There was an error on username or password';
+    }
+  }
+
+  return {
+    success: isGetValid,
+    errors
+  };
+}
+
+
+
 auth.post('/login', (req, res, next) => {
-  console.log('post request', req.body);
   const validationResult = validateLoginForm(req.body);
   if (!validationResult.success) {
-    console.log('errors', validationResult);
     return res.status(400).json({
       success: false,
-      message: validationResult.message,
       errors: validationResult.errors
     });
   }
-  console.log('alles klar');
+
+  const validationGetResult = getUser(req.body);
+  if (!validationGetResult.success) {
+    return res.status(400).json({
+      success: false,
+      errors: validationGetResult.errors
+    });
+  }
   return res.status(200).end();
 });
 
 auth.post('/signup', (req, res, next) => {
-  console.log('post request', req.body);
-  const validationResult = validateSignupForm(req.body);
-  if (!validationResult.success) {
-    console.log('errors', validationResult);
+  const validationFormResult = validateSignupForm(req.body);
+  if (!validationFormResult.success) {
     return res.status(400).json({
       success: false,
-      message: validationResult.message,
-      errors: validationResult.errors
+      errors: validationFormResult.errors
     });
   }
-  console.log('alles klar');
-  return res.status(200).end();
 
+  const validationDBResult = createUser(req.body);
+  if (!validationDBResult.success) {
+    return res.status(400).json({
+      success: false,
+      errors: validationFormResult.errors
+    });
+  }
+  return res.status(200).end();
 });
 
 module.exports = auth;
