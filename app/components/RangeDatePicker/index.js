792d6faf84dd35285027c7afc3b95e163b791ad3
Arnaud <arnaud.brun.vidal@gmail.com>
2018-05-18 17:25:31 +0200

    Add a date component to the homepage

---
+++ app/components/RangeDatePicker/index.js
@@ -0,0 +1,46 @@
+/**
+*
+* RangeDatePicker
+*
+*/
+
+import React from 'react';
+// import styled from 'styled-components';
+import 'react-dates/initialize';
+import { DateRangePicker, SingleDatePicker, DayPickerRangeController } from 'react-dates';
+
+import 'react-dates/lib/css/_datepicker.css';
+
+
+class RangeDatePicker extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  constructor(props) {
+    super(props);
+    this.state = {
+      startDate: null,
+      endDate: null,
+      focusedInput: null,
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <DateRangePicker
+          startDate={this.state.startDate}
+          startDateId="your_unique_start_date_id"
+          endDate={this.state.endDate}
+          endDateId="your_unique_end_date_id"
+          onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })}
+          focusedInput={this.state.focusedInput}
+          onFocusChange={focusedInput => this.setState({ focusedInput })}
+        />
+      </div>
+    );
+  }
+}
+
+RangeDatePicker.propTypes = {
+
+};
+
+export default RangeDatePicker;
---
+++ app/components/RangeDatePicker/messages.js
@@ -0,0 +1,13 @@
+/*
+ * RangeDatePicker Messages
+ *
+ * This contains all the text for the RangeDatePicker component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  header: {
+    id: 'app.components.RangeDatePicker.header',
+    defaultMessage: 'This is the RangeDatePicker component !',
+  },
+});
